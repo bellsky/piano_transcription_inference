8cefe5bd74e755173112dd8872eed1011c2d791c
bellsky <always1sky@gmail.com>
2023-01-12 10:19:44 +0900

    change package name to 'piano_transcription_bellsky'

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="piano-transcription-inference", # Replace with your own username
-    version="0.0.5",
+    name="piano-transcription-bellsky", # Replace with your own username
+    version="0.0.51",
     author="Qiuqiang Kong",
-    author_email="qiuqiangkong@gmail.com",
+    author_email="always1skyg@gmail.com",
     description="Piano transcription inference toolbox",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/bellsky/piano_transcription_inference",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
