1ac6e70deb2fea499b5cd4203643bce5ef1f324f
bellsky <112685337+bellsky@users.noreply.github.com>
2023-01-13 09:34:14 +0900

    deleted torchlibrosa in requires

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['matplotlib', 'mido', 'librosa>=0.9.0', 'git+https://github.com/bellsky/torchlibrosa'],
+    install_requires=['matplotlib', 'mido', 'librosa>=0.9.0'],
     python_requires='>=3.6',
 )
